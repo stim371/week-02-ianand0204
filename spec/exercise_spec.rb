d3ba3598ddbfa263b17d6af2ac9144e776f37300
Ian A <ianand0204@gmail.com>
2011-10-11 20:16:28 -0700

    look ma, it fails!

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -2,10 +2,10 @@
 
 describe "Strings" do
   context "when calling split" do
-    it "should split on whitespace by default"
+    it "should split on whitespace by default" do
 
     saying = "Didn't the quick brown fox jump over the lazy dog?"
-    saying.split.count.should == 9
-
+    saying.split("'").count.should == 10
+    end
   end
 end
