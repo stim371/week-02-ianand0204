ee585a936eb262f22636b618400676475b28f450
Joel Stimson <stimsojr@gmail.com>
2011-10-11 20:25:14 -0700

    fixed test and added 1 more

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -5,7 +5,13 @@ describe "Strings" do
     it "should split on whitespace by default" do
 
     saying = "Didn't the quick brown fox jump over the lazy dog?"
-    saying.split("'").count.should == 10
+    saying.split(" ").count.should == 10
+    end
+    
+    it "should split based on given character" do
+
+    saying = "Didn't the quick brown fox jump over the lazy dog?"
+    saying.split("'").count.should == 2
     end
   end
 end
